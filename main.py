d9180f9963b8e19eb396446ae91ce73d97aaccbc
Jon Ginter <jonginter28@gmail.com>
2023-11-15 09:01:49 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# Import statements
+import argparse
+import os
+import re
+
+# Parse argument to check validity
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Process arguments.")
+
+    # All arguments
+    parser.add_argument('-i', type=str, requried=True, help='The input file path')
+    parser.add_argument('-s', type=str, required=True, help='The seed string')
+    parser.add_argument('-o', type=str, required=True, help='The output file path')
+    parser.add_argument('-p', type=int, required=False, help='The number of processes')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Check if the input file exits || returns true if file exists
+    if not os.path.isfile(args.i):
+        raise argparse.ArgumentTypeError(f"Input file {args.i} does not exist")
+
+    # Check if seed string only has a, b, and c || returns match
+    if not re.match("^[abc]+$", args.s):
+        raise argparse.ArgumentTypeError(f"Seed string {args.s} is invalid, must only contain a, b, or c")
+
+    # Checking if the output file path exists || returns true if directory exists
+    if not os.path.isdir(os.path.dirname(args.o)):
+        raise argparse.ArgumentTypeError(f"Output directory {os.path.dirname(args.o)} does not exist")
+
+    # Checking if number of processes is greater than 0
+    if args.p <= 0:
+        raise argparse.ArgumentTypeError(f"Number of processes {args.p} is invalid, must be greater than 0")
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
