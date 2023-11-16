aefbbff8a0ecad88a187300ed6fcf3ead6be995b
Jon Ginter <jonginter28@gmail.com>
2023-11-16 11:44:45 -0600

    Prints out the arguments given

--- main.py
+++ main.py
@@ -19,24 +19,26 @@ def parse_arguments():
 
     # Check if the input file exits || returns true if file exists
     if not os.path.isfile(args.i):
-        raise argparse.ArgumentTypeError("Input file {args.i} does not exist")
+        raise argparse.ArgumentTypeError("Input file {} does not exist".format(args.i))
 
     # Check if seed string only has a, b, and c || returns match
     if not re.match("^[abc]+$", args.s):
-        raise argparse.ArgumentTypeError("Seed string {args.s} is invalid, must only contain a, b, or c")
+        raise argparse.ArgumentTypeError("Seed string {} is invalid, must only contain a, b, or c".format(args.s))
 
     # Checking if the output file path exists || returns true if directory exists
     if not os.path.isdir(os.path.dirname(args.o)):
-        raise argparse.ArgumentTypeError("Output directory {os.path.dirname(args.o)} does not exist")
+        raise argparse.ArgumentTypeError("Output directory {} does not exist".format(os.path.dirname(args.o)))
 
     # Checking if number of processes is greater than 0
     if args.p <= 0:
-        raise argparse.ArgumentTypeError("Number of processes {args.p} is invalid, must be greater than 0")
+        raise argparse.ArgumentTypeError("Number of processes {} is invalid, must be greater than 0".format(args.p))
+
+    return args
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print('Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Hi, {}'.format(name))  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
